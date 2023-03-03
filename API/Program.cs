bfddfa4e0f52f06facf43f9325ebd3e5cb143a61
Astha Shrestha <aashthaxtha046@gmail.com>
2023-03-03 14:40:26 +0545

    End of Section 3, working environment is created

--- API/Program.cs
+++ API/Program.cs
@@ -11,10 +11,12 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 //configure the HTTP request pipeline
-
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
