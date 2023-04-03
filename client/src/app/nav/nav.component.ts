f2f58b3d40f0f07529fddbe0ba80c90d53b949d2
Astha Shrestha <aashthaxtha046@gmail.com>
2023-04-03 11:16:46 +0545

    Angular Theme added and shared module

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable, of } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -13,34 +15,25 @@ export class NavComponent implements OnInit {
 
   // currentUser$: Observable<User | null> = of(null);
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router, private toastr:ToastrService) { }
 
   ngOnInit(): void {
     // this.getCurrentUser();
   }
 
-  // getCurrentUser(){
-  //   this.accountService.currentUser$.subscribe({
-  //     next: user => this.loggedIn = !!user,
-  //     error: error => console.log(error)
-  //   })
-  // }
-
+  
   login(){
-    //console.log(this.model);
+   
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        console.log(response);
-        // this.loggedIn = true;
-      },
-      error: error => console.log(error)
+      next: _ => this.router.navigateByUrl('/members'),
+      error: error => this.toastr.error(error.error)
       
     })
   }
 
   logout(){
     this.accountService.logout();
-    // this.loggedIn = false;
+    this.router.navigateByUrl('/');
   }
 
 }
